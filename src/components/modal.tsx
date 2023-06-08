d4ec4b8c62d444f31059b9269b408aa794d2b3a5
Rageboom <rageboom@gmail.com>
2023-06-08 00:57:14 +0000

    모달 UI 수정.

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,8 +1,8 @@
 import { ReactNode } from "react";
 import { useRecoilState } from "recoil";
 import { modalState } from "../store/modalState";
-import "./modal.scss";
 import { useHideModal } from "../hooks/modalHook";
+import "./modal.scss";
 
 type ModalProps = {
   children?: ReactNode;
@@ -11,21 +11,30 @@ type ModalProps = {
 function Modal({ children }: ModalProps) {
   const [modal] = useRecoilState(modalState);
   const hideModal = useHideModal();
-  const handleCloseClick = () => {
-    hideModal();
+  const handleCloseClick = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
+    hideModal(e.currentTarget);
   };
   return (
-    <div className={`modal ${modal.show ? "show" : "hide"}`}>
-      <div className="modal-bg" onClick={handleCloseClick}></div>
+    <div
+      className={`modal ${
+        modal.show === null ? "" : modal.show ? "show" : "hide"
+      }`}
+    >
+      <div
+        className="modal-bg"
+        style={{ transformOrigin: `${modal.pos.x}px ${modal.pos.y}px` }}
+      ></div>
       <div className="modal-layout">
-        <div className="modal-title">
-          <div className="title">{modal.title}</div>
-          <button onClick={handleCloseClick}>
-            <i className="fa-solid fa-xmark"></i>
-          </button>
+        <button className='modal-close' onClick={handleCloseClick}>
+          <i className="fa-solid fa-xmark"></i>
+        </button>
+        <div className="modal-form">
+          <div className="modal-title">
+            <div className="title">{modal.title}</div>
+          </div>
+          <div className="modal-content">{modal.content}</div>
+          {children}
         </div>
-        <div className="modal-content">{modal.content}</div>
-        {children}
       </div>
     </div>
   );
